fd295edadeeaf9d858168a5263265cee22ce4468
Dmitriy Kolach <lobjob@rambler.ru>
2021-09-13 01:53:20 +0300

    add changes

--- execution files/.ipynb_checkpoints/predict_weather-checkpoint.py
+++ execution files/.ipynb_checkpoints/predict_weather-checkpoint.py
@@ -1,19 +1,24 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_absolute_error
-from sklearn.dummy import DummyRegressor
 from catboost import CatBoostRegressor
 from notifiers import get_notifier
 
-def new_time_features(df, key_column):
+def new_time_features(df, max_temp_col, min_temp_col, press_col, hum_col):
+    all_columns = [max_temp_col, min_temp_col, press_col, hum_col]
+    temp_columns = [max_temp_col, min_temp_col]
     df['month'] = df.index.month
     df['day'] = df.index.day
-    df['rolling_mean'] = df[key_column].shift(7).rolling(7).mean()
-    df['lag_7'] = df[key_column].shift(7)
-    df['lag_15'] = df[key_column].shift(15)
-    df['lag_30'] = df[key_column].shift(30)
-    df['lag_90'] = df[key_column].shift(90)
-    df['lag_180'] = df[key_column].shift(180)
+    
+    for col in all_columns:
+        df['rolling_mean_'+col] = df[col].shift(7).rolling(10).mean()
+        
+    df['lag_7_max_temp'] = df[max_temp_col].shift(7)
+    df['lag_20_max_temp'] = df[max_temp_col].shift(20)
+    
+    for col in temp_columns:
+        df['lag_15_'+col] = df[col].shift(15)
+        df['lag_180_'+col] = df[col].shift(180)
     return df
 
 def search_best_model(model, model_name, params, X, y):
@@ -28,16 +33,14 @@ def test_and_score(model):
 
 def make_predictions(df, model):
     df_pred = df.iloc[-7:]
-    X_pred = df_pred.drop('temperature', axis=1)
+    X_pred = df_pred.drop('max_temp', axis=1)
     preds = best_cb_model.predict(X_pred)
     
-    columns_to_drop = ['month', 'day', 'rolling_mean', 'lag_7',
-                       'lag_15', 'lag_30', 'lag_90', 'lag_180']
     X_pred = X_pred.copy()
     X_pred['День недели'] = X_pred.index.day_name()
     X_pred['Температура'] = preds
     X_pred['Температура'] = X_pred['Температура'].round(1)
-    X_pred = X_pred.drop(columns_to_drop, axis=1)
+    X_pred = X_pred[['День недели', 'Температура']]
     
     days_week_rus = {
         'Monday':'Понедельник', 'Tuesday':'Вторник', 'Wednesday':'Среда',
@@ -48,8 +51,8 @@ def make_predictions(df, model):
     return X_pred
 
 def telegram_notifier_to_channel(
-    token='<bot_token>',
-    chat_id='@<chanel_name>'
+    token='<telegram-bot token>',
+    chat_id='@<channel name>'
 ):
     def f(text):
         telegram = get_notifier('telegram')
@@ -61,8 +64,8 @@ def telegram_notifier_to_channel(
     return f
 
 def telegram_notifier_to_bot(
-    token='<bot_token>',
-    chat_id='<chat_with_bot_id>'
+    token='<telegram-bot token>',
+    chat_id='<chat_with_bot_number>'
 ):
     def f(text):
         telegram = get_notifier('telegram')
@@ -73,10 +76,11 @@ def telegram_notifier_to_bot(
         )
     return f
 
-df_all = pd.read_csv('Datasets/weather/weather.csv', index_col=[0], parse_dates=[0], dayfirst=True)
+df_all_final = pd.read_csv('Datasets/weather/weather.csv', index_col=[0], parse_dates=[0], dayfirst=True)
+df_all = df_all_final.drop(['min_temp', 'pressure', 'humidity'], axis=1)
 
-X = df_all.drop('temperature', axis=1)
-y = df_all['temperature']
+X = df_all.drop('max_temp', axis=1)
+y = df_all['max_temp']
 X, X_test, y, y_test = train_test_split(X, y, test_size=0.05, shuffle=False)
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.055, shuffle=False)
 
@@ -96,14 +100,15 @@ send_bot(
     .format(best_cb_score, test_and_score(best_cb_model))
 )
 
-X = df_all.drop('temperature', axis=1)
-y = df_all['temperature']
+X = df_all.drop('max_temp', axis=1)
+y = df_all['max_temp']
 best_cb_model.fit(X, y)
 
-next_7_dates = pd.Series(pd.date_range(df_all.index[-1], periods=7, freq='D').shift().normalize())
-df_all_next = pd.DataFrame(index=next_7_dates, columns=df_all.columns)
-df_all = pd.concat([df_all, df_all_next])
-df_all = new_time_features(df_all, 'temperature')
+next_7_dates = pd.Series(pd.date_range(df_all_final.index[-1], periods=7, freq='D').shift().normalize())
+df_all_next = pd.DataFrame(index=next_7_dates, columns=df_all_final.columns)
+df_all = pd.concat([df_all_final, df_all_next])
+new_time_features(df_all, 'max_temp', 'min_temp', 'pressure', 'humidity')
+df_all = df_all.drop(['min_temp', 'pressure', 'humidity'], axis=1)
 
 predictions_temperature = make_predictions(df_all, best_cb_model)
 predictions_text = ('Предсказания на неделю:'
@@ -135,3 +140,4 @@ predictions_text = ('Предсказания на неделю:'
                    )
 send_channel = telegram_notifier_to_channel()
 send_channel(predictions_text)
+#send_bot(predictions_text)
--- execution files/weather_prepare.py
+++ execution files/weather_prepare.py
@@ -7,15 +7,21 @@ def load_csv(path):
     ).sort_index(ascending=True)
     return df
 
-def new_time_features(df, key_column):
+def new_time_features(df, max_temp_col, min_temp_col, press_col, hum_col):
+    all_columns = [max_temp_col, min_temp_col, press_col, hum_col]
+    temp_columns = [max_temp_col, min_temp_col]
     df['month'] = df.index.month
     df['day'] = df.index.day
-    df['rolling_mean'] = df[key_column].shift(7).rolling(7).mean()
-    df['lag_7'] = df[key_column].shift(7)
-    df['lag_15'] = df[key_column].shift(15)
-    df['lag_30'] = df[key_column].shift(30)
-    df['lag_90'] = df[key_column].shift(90)
-    df['lag_180'] = df[key_column].shift(180)
+    
+    for col in all_columns:
+        df['rolling_mean_'+col] = df[col].shift(7).rolling(10).mean()
+        
+    df['lag_7_max_temp'] = df[max_temp_col].shift(7)
+    df['lag_20_max_temp'] = df[max_temp_col].shift(20)
+    
+    for col in temp_columns:
+        df['lag_15_'+col] = df[col].shift(15)
+        df['lag_180_'+col] = df[col].shift(180)
     return df
 
 new_columns = [
@@ -31,19 +37,33 @@ df_2 = load_csv('Datasets/weather/weather_2010-2015.csv')
 df_3 = load_csv('Datasets/weather/weather_2015-2020.csv')
 df_4 = load_csv('Datasets/weather/weather_last.csv')
 
-df_1 = df_1['температура'].to_frame().astype('float')
-df_2 = df_2['температура'].to_frame().astype('float')
-df_3 = df_3['температура'].to_frame().astype('float')
-df_4 = df_4['температура'].to_frame().astype('float')
+df_1 = df_1[['температура', 'атм_давл_на_ст', 'отн_влаж']]
+df_2 = df_2[['температура', 'атм_давл_на_ст', 'отн_влаж']]
+df_3 = df_3[['температура', 'атм_давл_на_ст', 'отн_влаж']]
+df_4 = df_4[['температура', 'атм_давл_на_ст', 'отн_влаж']]
 
 df_all = pd.concat([df_1, df_2, df_3, df_4])
-df_all = df_all.resample('1D').max()
-df_all = df_all.rename(columns={'температура':'temperature'})
+df_all_max_temp = df_all['температура'].to_frame().astype('float')
+df_all_final = df_all_max_temp.resample('1D').max()
+df_all_final = df_all_final.rename(columns={'температура':'max_temp'})
 
-df_all.loc['2008-05-06'] = df_all.loc['2008-05-06'].fillna(df_all.loc['2008-05-04':'2008-05-08'].mean())
-df_all.loc['2012-12-16'] = df_all.loc['2012-12-16'].fillna(df_all.loc['2012-12-14':'2012-12-18'].mean())
+df_all_min_temp = df_all['температура'].to_frame().astype('float')
+df_all_min_temp = df_all_min_temp.resample('1D').min()
+df_all_min_temp = df_all_min_temp.rename(columns={'температура':'min_temp'})
+df_all_final = df_all_final.join(df_all_min_temp, how='left')
 
-new_time_features(df_all, 'temperature')
-df_all = df_all.dropna()
+columns_for_remake = ['атм_давл_на_ст', 'отн_влаж']
+for col in columns_for_remake:
+    df_all_new_column = df_all[col].to_frame().astype('float')
+    df_all_new_column = df_all_new_column.resample('1D').mean().round(1)
+    df_all_final = df_all_final.join(df_all_new_column, how='left')
+df_all_final = df_all_final.rename(columns={'атм_давл_на_ст':'pressure', 'отн_влаж':'humidity'})
 
-df_all.to_csv('Datasets/weather/weather.csv')
+df_all_final.loc['2008-05-06'] = df_all_final.loc['2008-05-06'].fillna(
+    df_all_final.loc['2008-05-04':'2008-05-08'].median())
+df_all_final.loc['2012-12-16'] = df_all_final.loc['2012-12-16'].fillna(
+    df_all_final.loc['2012-12-14':'2012-12-18'].median())
+new_time_features(df_all_final, 'max_temp', 'min_temp', 'pressure', 'humidity')
+df_all_final = df_all_final.dropna()
+
+df_all_final.to_csv('Datasets/weather/weather.csv')
